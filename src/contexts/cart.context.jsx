f117742465eafcc19379c660be08973d2ba296d8
Jeff Hedgpeth <jeff@elevatetickets.com>
2023-09-01 23:43:54 -0700

    Adding Cart Context, CartIcon, CartDropdown

---
+++ src/contexts/cart.context.jsx
@@ -0,0 +1,31 @@
+import { createContext, useState, useEffect } from "react";
+
+export const CartContext = createContext({
+  isCartOpen: false,
+  setIsCartOpen: () => {},
+  cartItems: [],
+  addItemToCart: () => {},
+  removeItemFromCart: () => {},
+  clearItemFromCart: () => {},
+  cartCount: 0,
+  cartTotal: 0,
+});
+
+export const CartProvider = ({ children }) => {
+  const [isCartOpen, setIsCartOpen] = useState(false);
+  const [cartItems, setCartItems] = useState([]);
+  const [cartCount, setCartCount] = useState(0);
+  const [cartTotal, setCartTotal] = useState(0);
+  
+  const value = { cartItems, setIsCartOpen, isCartOpen };
+
+  useEffect(() => {
+    setCartItems(cartItems);
+  }, []);
+
+  return (
+    <CartContext.Provider value={value}>
+      {children}
+    </CartContext.Provider>
+  );
+};
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -3,12 +3,15 @@ import { Outlet, Link } from "react-router-dom";
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
-
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 import "./navigation.styles.scss";
+import { CartContext } from "../../contexts/cart.context";
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
-
+    const { isCartOpen, setIsCartOpen } = useContext(CartContext);
+    console.log(isCartOpen);
     return (
       <Fragment>
         <div className="navigation">
@@ -28,6 +31,8 @@ const Navigation = () => {
                 SIGN IN
               </Link>
             )}
+            <CartIcon />
+            {isCartOpen && <CartDropdown />}
           </div>
         </div>
         <Outlet />
